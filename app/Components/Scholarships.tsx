e7c8904e5956a3013411b0006d90d497d132d2ac
Spencer Hepworth <spenstar@Spencers-MacBook-Pro.local>
2023-06-28 17:24:30 -0600

    finished the scholarships and africa sections

---
+++ app/Components/Scholarships.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+
+interface Student {
+    imageUrl: string,
+    name: string,
+    highschoolName: string,
+    collegeName: string
+}
+
+export const StudentData: Student[] = [
+    {
+        imageUrl: '/impact/our-community-page/tanner.png',
+        name: 'Tanner Schroeder',
+        highschoolName: 'Declo High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/cassie.png',
+        name: 'Cassie Beene',
+        highschoolName: 'Burley High School',
+        collegeName: 'Montana State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/jonathan.png',
+        name: 'Jonathan Bateman',
+        highschoolName: 'Declo High School',
+        collegeName: 'Brigham Young University - Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/emma.png',
+        name: 'Emma Jeske',
+        highschoolName: 'Oakley High School',
+        collegeName: 'College of Southern Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/tate.png',
+        name: 'Tate Cranney',
+        highschoolName: 'Oakley High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/brittany.png',
+        name: 'Brittany Hardy',
+        highschoolName: 'Oakley High School',
+        collegeName: 'Bridgerland Technical College'
+    },
+    {
+        imageUrl: '/impact/our-community-page/elise.png',
+        name: 'Elise Simkins',
+        highschoolName: 'Declo High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/melanie.png',
+        name: 'Melanie Searle',
+        highschoolName: 'Burley High School',
+        collegeName: 'College of Southern Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/jonathan2.png',
+        name: 'Jonathan Brumley',
+        highschoolName: 'Minico High School',
+        collegeName: 'University of Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/zac.png',
+        name: 'Zac Powers',
+        highschoolName: 'Raft River High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/colby.png',
+        name: 'Colby Wilkins',
+        highschoolName: 'Minico High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/aymslee.png',
+        name: 'Aymslee Mathis',
+        highschoolName: 'Burley High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/tegan.png',
+        name: 'Tegan Whitaker',
+        highschoolName: 'Raft River High School',
+        collegeName: 'Bridgerland Technical College'
+    },
+    {
+        imageUrl: '/impact/our-community-page/ben.png',
+        name: 'Ben Jones',
+        highschoolName: 'Oakley High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/courtney.png',
+        name: 'Courtney Beene',
+        highschoolName: 'Burley High School',
+        collegeName: 'College of Southern Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/rylie.png',
+        name: 'Rylie Ann Nalder',
+        highschoolName: 'Mince High School',
+        collegeName: 'Brigham Young University - Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/j-w.png',
+        name: 'J.W. Powers',
+        highschoolName: 'Raft River High School',
+        collegeName: 'College of Southern Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/melissa.png',
+        name: 'Melissa Spurgeon',
+        highschoolName: 'Declo High School',
+        collegeName: 'College of Southern Idaho'
+    },
+    {
+        imageUrl: '/impact/our-community-page/klayton.png',
+        name: 'Klayton Wilson',
+        highschoolName: 'Minco High School',
+        collegeName: 'Utah State University'
+    },
+    {
+        imageUrl: '/impact/our-community-page/kaylee.png',
+        name: 'Kaylee Whiteley',
+        highschoolName: 'Oakley High School',
+        collegeName: 'College of Southern Idaho'
+    },
+];
+
+export default function Students() {
+    return (
+        StudentData.map((student, index) => (
+            <div key={index} className="flex flex-col items-center">
+                <img src={`${student.imageUrl}`} className="w-48 pb-2"/>
+                <p>{student.name}</p> 
+                <p>{student.highschoolName}</p> 
+                <p>{student.collegeName}</p> 
+            </div>
+        ))
+    );
+}
--- app/impact/our-community/page.tsx
+++ app/impact/our-community/page.tsx
@@ -1,3 +1,5 @@
+import Students from "@/app/Components/Scholarships";
+
 export default function OurCommunity() {
 
     return (
@@ -12,6 +14,28 @@ export default function OurCommunity() {
                     High Desert Milk Scholarship
                 </h1>
                 <button className="flex bg-home-blue items-center text-white text-md font-light pl-40 rounded-r-md hover:bg-blue-900 active:text-home-blue active:bg-white active:border-home-blue active:border-2">Apply For 2023 Here <img src="../side-arrow.png" className="w-8"/></button>
+            </div>
+            <div className="p-24 grid grid-cols-[250px_250px_250px] gap-5 text-[10px] leading-tight justify-center grid-cols- font-sans text-home-blue">
+                <Students/>
+            </div>
+            <div className="bg-home-orange text-home-blue">
+                <div className="grid grid-cols-2">
+                    <div className="flex flex-col p-8 justify-center items-center max-w-[600px]">
+                        <h1 className="text-4xl font-light p-8 min-w-[270px]">Africa Outreach</h1>
+                        <p className="p-8 text-sm">Every year, High Desert Milk donates pallets 
+of our powdered milk in Zimbabwe. As you can 
+see from these photos, the natives love being 
+able to enjoy the flavor and nutrients that it 
+provides - most of them not having tasted a 
+glasss of milk since they were infants. Makes 
+you ask yourself - was I able to enjoy a glass of 
+milk today?</p>
+
+                    </div>
+                    <div>
+                        <img src="../impact/our-community-page/africa.png" alt="" />
+                    </div>
+                </div>
             </div>
         </div>
     )
