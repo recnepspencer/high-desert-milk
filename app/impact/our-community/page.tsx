ddd821d41e0e0d702faf253c58213b30238e351a
Spencer Hepworth <spenstar@Spencers-MacBook-Pro.local>
2023-06-28 16:03:05 -0600

    fixed errors

---
+++ app/impact/our-community/page.tsx
@@ -0,0 +1,18 @@
+export default function OurCommunity() {
+
+    return (
+        <div>
+            <div className="pt-[148px]"></div>
+            <div className="relative">
+                <img src="../impact/our-community-page/our-community-bg.png" alt="" className="w-full"/>
+                <h1 className="absolute bg-home-blue rounded-xl text-white text-5xl top-[65%] left-[50%] -translate-x-[50%] font-light p-4 w-[400px] mb-12"> Our Community</h1>
+            </div>
+            <div className="mt-10 mb-10">
+                <h1 className="text-4xl text-home-blue pl-8 mb-8">
+                    High Desert Milk Scholarship
+                </h1>
+                <button className="flex bg-home-blue items-center text-white text-md font-light pl-40 rounded-r-md hover:bg-blue-900 active:text-home-blue active:bg-white active:border-home-blue active:border-2">Apply For 2023 Here <img src="../side-arrow.png" className="w-8"/></button>
+            </div>
+        </div>
+    )
+}
